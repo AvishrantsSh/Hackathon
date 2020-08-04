19ef4413209e7cfab8710b96900c1fc89a68af62
Avishrant Sharma <avishrants@gmail.com>
2020-08-04 12:59:24 +0530

    Something Changed

--- central/views.py
+++ central/views.py
@@ -6,8 +6,7 @@ import json
 @csrf_exempt
 def newdt(request):
     if request.method == 'POST':
-        received_json_data = json.loads(request.body.decode("utf-8"))
-        return HttpResponse(received_json_data+"Yahi hai?")    
+        return HttpResponse(request.POST['name']+"Yahi hai?")    
     
     return HttpResponse("Lag Gaye")
 # Create your views here.
