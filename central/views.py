625fa374ea4c0447e4936cded61831851541c3b1
Avishrant Sharma <avishrants@gmail.com>
2020-08-04 22:15:05 +0530

    Fixed at last

--- central/views.py
+++ central/views.py
@@ -7,20 +7,20 @@ import json
 @csrf_exempt
 def newdt(request):
     if request.method == 'POST':
-        try:
-            if request.POST['h_name'] and request.POST['age'] and request.POST['action']:
-                new_record = Hospital_Records.objects.get(name=request.POST['h_name'])
-                new_record.address = request.POST['action']
-                new_record.available -= 1
-                new_record.save()
+        # try:
+        #     if request.POST['h_name'] and request.POST['age'] and request.POST['action']:
+        #         new_record = Hospital_Records.objects.get(name=request.POST['h_name'])
+        #         new_record.address = request.POST['action']
+        #         new_record.available -= 1
+        #         new_record.save()
 
-                # Age Stuff
-                age = Age_Freq.objects.get(age=request.POST['age'])
-                age.frequency += 1
-                age.save()
-                return HttpResponse("Nice Boi")
-        except:    
-            return HttpResponse("I Ka Bhej Diye Ho")               
-    
+        #         # Age Stuff
+        #         age = Age_Freq.objects.get(age=request.POST['age'])
+        #         age.frequency += 1
+        #         age.save()
+        #         return HttpResponse("Nice Boi")
+        # except:    
+        #     return HttpResponse("I Ka Bhej Diye Ho")               
+        return HttpResponse(str(request.POST)+" ye mila mujhe")
     return HttpResponse("Lag Gaye")
 # Create your views here.
