7e982558ffcc799aa31edd8fe7a2755bfb420a2d
Avishrant Sharma <avishrants@gmail.com>
2020-08-05 15:38:55 +0530

    Improve

--- central/views.py
+++ central/views.py
@@ -7,20 +7,18 @@ import json
 @csrf_exempt
 def newdt(request):
     if request.method == 'POST':
-        try:
-            if request.POST['h_name'] and request.POST['age'] and request.POST['action']:
-                new_record = Hospital_Records.objects.get(name=request.POST['h_name'])
-                new_record.address = request.POST['action']
-                new_record.available -= 1
-                new_record.total += 1
-                new_record.save()
+        # try:
+        if request.POST['h_name'] and request.POST['age'] and request.POST['action']:
+            new_record = Hospital_Records.objects.get(name=request.POST['h_name'])
+            new_record.address = request.POST['action']
+            new_record.available -= 1
+            new_record.total += 1
+            new_record.save()
 
-                # Age Stuff
-                return HttpResponse("Nice Boi")
-        except:    
-            return HttpResponse("I Ka Bhej Diye Ho")               
-        return HttpResponse(str(request.body)+" ye mila mujhe" + str(type(request.body)))
-
+            # Age Stuff
+            return HttpResponse("Nice Boi")
+        # except:    
+            # return HttpResponse("I Ka Bhej Diye Ho")
     return HttpResponse("Lag Gaye")
 
 def random_gen(request):
