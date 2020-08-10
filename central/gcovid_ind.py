20b674cf0c361817c14bd8c4535d657eeb693a2c
Avishrant Sharma <avishrants@gmail.com>
2020-08-10 23:03:33 +0530

    Init Responsive Charts

--- central/gcovid_ind.py
+++ central/gcovid_ind.py
@@ -8,6 +8,7 @@ from datetime import datetime, date
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 import plotly.offline as py
+import plotly.graph_objs as go
 
 class Covid_map(object):
     def __init__(self):
@@ -51,11 +52,11 @@ class Covid_map(object):
         df = df[:][['dailyconfirmed', 'date']]
         df = df.rename(columns = {'dailyconfirmed': 'new_cases', 'dailydeceased':'fatalities'})
         df['new_cases'] = df['new_cases'].astype(int)
+        tmp = np.array(df['new_cases'])
         df['date'] = pd.to_datetime(df.date)
         df = df.reset_index(drop=False)
         df['cap']=100000
         df.columns = ['index', 'y', 'ds','cap']
-
         m = Prophet(growth="logistic")
         m.fit(df)
         future = m.make_future_dataframe(periods=20)
@@ -64,6 +65,60 @@ class Covid_map(object):
         forecast = m.predict(future)
         forecast['trend'] = forecast['trend'].apply(np.ceil)
         forecast['trend'] = forecast['trend'].astype(int)
-        
-        fig = plot_plotly(m, forecast)  # This returns a plotly Figure
-        return py.plot(fig, output_type='div', include_plotlyjs=False,show_link=False, link_text="")
+        ##Method-1
+        # fig = plot_plotly(m, forecast)  # This returns a plotly Figure
+        # return py.plot(fig, output_type='div', include_plotlyjs=False,show_link=False, link_text="")
+
+        ##Method-2
+        yhat = go.Scatter(
+                        x = forecast['ds'],
+                        y = forecast['yhat'],
+                        mode = 'lines',
+                        marker = {
+                            'color': '#3bbed7'
+                        },
+                        line = {
+                            'width': 3
+                        },
+                        name = 'Forecast',
+                        )
+
+        yhat_upper = go.Scatter(
+                                x = forecast['ds'],
+                                y = tmp,
+                                marker=dict(color="crimson", size=5),
+                                mode="markers",
+                                name = 'Actual',
+
+                                )
+
+
+
+        layout = go.Layout(
+                        yaxis = {'title': {'text': 'y'}},
+                        hovermode = 'x',
+                        xaxis = {'rangeselector': {'buttons': [{'count': 7, 'label': '1w', 'step': 'day', 'stepmode': 'backward'},
+                                                                            {'count': 1,
+                                                                                'label': '1m',
+                                                                                'step': 'month',
+                                                                                'stepmode': 'backward'},
+                                                                            {'count': 6,
+                                                                                'label': '6m',
+                                                                                'step': 'month',
+                                                                                'stepmode': 'backward'},
+                                                                            {'count': 1, 'label': '1y', 'step': 'year', 'stepmode': 'backward'},
+                                                                            {'step': 'all'}]},
+                                                'rangeslider': {'visible': True},
+                                                'title': {'text': 'ds'},
+                                                'type': 'date'},
+                        
+                        legend = {
+                            'bgcolor': 'rgba(0,0,0,0)'
+                        },
+                        autosize=True,
+                       
+                        )
+
+        data = [yhat_upper, yhat]
+        fig = dict(data = data, layout = layout)
+        return py.plot(fig, output_type='div', include_plotlyjs=False,show_link=False, link_text="", image_width="500px")
