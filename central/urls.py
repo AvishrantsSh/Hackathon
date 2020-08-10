15ad72f027bf29d27589acc925da431a3252eeec
Avishrant Sharma <avishrants@gmail.com>
2020-08-10 21:07:03 +0530

    GodSpeed

--- central/urls.py
+++ central/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('add/',views.newdt,name='add'),
     path('random/', views.random_gen, name='random'),
     path('generate/', TemplateView.as_view(template_name='generate.html'), name='generate'),
-    # path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
-    # path('manifest.json',TemplateView.as_view(template_name='pwa/manifest.json',content_type='text/plain')),
-    # path('serviceworker.js',TemplateView.as_view(template_name='pwa/serviceworker.js',content_type='text/javascript')),
+    path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path('manifest.json',TemplateView.as_view(template_name='pwa/manifest.json',content_type='text/plain')),
+    path('serviceworker.js',TemplateView.as_view(template_name='pwa/serviceworker.js',content_type='text/javascript')),
 ]
