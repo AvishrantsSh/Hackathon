21f6e96c992c6d08814664517ddb398f5a8f8a45
Avishrant Sharma <avishrants@gmail.com>
2020-08-15 18:44:21 +0530

    Added Research Field

--- templates/pwa/serviceworker.js
+++ templates/pwa/serviceworker.js
@@ -3,6 +3,10 @@ const CACHE = 'cache_v621j';
 const filestoCache = ['/static/noconn.html',
                       'https://cdn.plot.ly/plotly-latest.min.js',
                       '/static/style/basestyle.css',
+                      '/static/style/homestyle.css',
+                      '/static/style/statstyle.css',
+                      '/static/style/loginstyle.css',
+
                       '/manifest.json',
                       '/static/images/assets/Avishrant.jpg',
                       '/static/images/assets/Kunal.jpg',
