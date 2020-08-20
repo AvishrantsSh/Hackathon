3831b3376b6a454e17916a16086b21ef8e7b5832
Avishrant Sharma <avishrants@gmail.com>
2020-08-20 18:14:21 +0530

    Updater fixes

--- updater.py
+++ updater.py
@@ -7,20 +7,17 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    # try:
-    print('Running scheduled Job')
-    module_dir = path.dirname(__file__)  
-    file_path = path.join(module_dir, 'central/temp.json')
-    fobj = open(file_path, "w")
-    response = requests.get(url = 'https://testplotly.herokuapp.com/getstats/general/covid/plotly/data.json')
-    # self.todos = response.text
-    fobj.write(str(response.text))
-    fobj.truncate()
-    fobj.close()
-    print("Job Completed Successfully")
-    fobj = open(file_path, 'r+')
-    print(fobj.readlines())
-    fobj.close()
-    # except:
-    #     print("Something went wrong")
+    try:
+        print('Running scheduled Job')
+        module_dir = path.dirname(__file__)  
+        file_path = path.join(module_dir, 'central/temp.json')
+        fobj = open(file_path, "w")
+        response = requests.get(url = 'https://testplotly.herokuapp.com/getstats/general/covid/plotly/data.json')
+        # self.todos = response.text
+        fobj.write(str(response.text))
+        fobj.truncate()
+        fobj.close()
+        print("Job Completed Successfully")
+    except:
+        print("Something went wrong")
 sched.start()
