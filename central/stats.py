ced619ed08c2116154474cc0501b2ed706807961
Avishrant Sharma <avishrants@gmail.com>
2020-08-20 17:59:21 +0530

    Updater fixes

--- central/stats.py
+++ central/stats.py
@@ -5,30 +5,26 @@ from datetime import datetime, date
 
 class covid_stats(object):
     def __init__(self):
-        pass
-
+        self.link = "https://testplotly.herokuapp.com/getstats/general/covid/plotly/data.json"
+        # self.link='http://192.168.43.15:8000/getstats/general/covid/plotly/data.json'
+    
     def get_stats(self):
         module_dir = os.path.dirname(__file__)  
         file_path = os.path.join(module_dir, 'temp.json')
-        update = Updater(file_path)
         fobj = open(file_path, "r+")
         if not fobj.read(1):
             fobj.close()
-            update.get_data()
+            self.get_data()
 
         fobj = open(file_path, "r+")
         jobj = dict(json.loads(fobj.read()))
         fobj.close()
         return jobj["Time"], jobj["Data"], jobj["TTotal"],jobj["TRecovered"],jobj["TDeceased"],jobj["DTotal"],jobj["DRecovered"],jobj["DDeceased"]
 
-class Updater(object):
-    def __init__(self, path):
-        self.link = "https://testplotly.herokuapp.com/getstats/general/covid/plotly/data.json"
-        self.path = path
-        # self.link='http://192.168.43.15:8000/getstats/general/covid/plotly/data.json'
-
     def get_data(self):
-        fobj = open(self.path, "r+")
+        module_dir = os.path.dirname(__file__)  
+        file_path = os.path.join(module_dir, 'temp.json')
+        fobj = open(file_path, "w")
         response = requests.get(url = self.link)
         fobj.write(str(response.text))
         fobj.truncate()
--- updater.py
+++ updater.py
@@ -1,21 +1,24 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from central.stats import Updater
 from django.core import serializers
-import json
+import json, requests
 from os import path
 from datetime import datetime
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', seconds=20)
 def timed_job():
-    try:
-        module_dir = path.dirname(path.abspath(__file__))  
-        file_path = path.join(module_dir, 'central/temp.json')
-        print('Running scheduled Job: Editing ', file_path)
-        obj = Updater(file_path)
-        obj.get_data()
-        print("Job Completed Successfully")
+    # try:
+    print('Running scheduled Job')
+    module_dir = path.dirname(__file__)  
+    file_path = path.join(module_dir, 'central/temp.json')
+    fobj = open(file_path, "w")
+    response = requests.get(url = 'https://testplotly.herokuapp.com/getstats/general/covid/plotly/data.json')
+    # self.todos = response.text
+    fobj.write(str(response.text))
+    fobj.truncate()
+    fobj.close()
+    print("Job Completed Successfully")
 
-    except:
-        print("Something went wrong")
+    # except:
+    #     print("Something went wrong")
 sched.start()
