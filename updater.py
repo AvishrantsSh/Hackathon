a90fadb688aeb053fe00b969f119993591928047
Avishrant Sharma <avishrants@gmail.com>
2020-08-20 17:43:39 +0530

    Updater fixes

--- updater.py
+++ updater.py
@@ -8,14 +8,14 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', seconds=20)
 def timed_job():
-    # try:
-    print('Running scheduled Job')
-    module_dir = path.dirname(__file__)  
-    file_path = path.join(module_dir, 'central/temp.json')
-    obj = Updater(file_path)
-    obj.get_data()
-    print("Job Completed Successfully")
+    try:
+        module_dir = path.dirname(path.abspath(__file__))  
+        file_path = path.join(module_dir, 'central/temp.json')
+        print('Running scheduled Job: Editing ', file_path)
+        obj = Updater(file_path)
+        obj.get_data()
+        print("Job Completed Successfully")
 
-    # except:
-    #     print("Something went wrong")
+    except:
+        print("Something went wrong")
 sched.start()
